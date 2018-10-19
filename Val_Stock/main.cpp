606d1839b2c05e20b3846cc0d172f8ed0d29c406
TTNguyen <nttriet9a4@gmail.com>
2018-10-19 21:03:46 +0700

    Initial Commit

---
+++ Val_Stock/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Val_Stock
+//
+//  Created by TT Nguyen on 10/19/18.
+//  Copyright © 2018 TT Nguyen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
